2912f8ce953b6fb721e4925cd92fc0bd725682b6
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-09-08 15:45:48 +0100

    Adding Static ROOT in local filesystem while app in development

--- TheLearningHub/settings.py
+++ TheLearningHub/settings.py
@@ -149,6 +149,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# TODO Add support hosted service for static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),

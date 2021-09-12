c3a93ec756c38fa45f6d735c004eeb43583206d2
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-09-12 22:32:05 +0100

    Adding edit/article path to urls.py - Articles

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name="articles"),
     path('<int:article_id>/', views.article, name="article"),
     path('delete/<int:article_id>/', views.delete_article, name='delete_article'),
-    # path('edit/<int:article_id>/', views.edit_article, name='edit_article'),
+    path('edit/<int:article_id>/', views.edit_article, name='edit_article'),
     path('write_article', views.write_article, name="write_article")
 ]

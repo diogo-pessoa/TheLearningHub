d5bb85bb23e0981f3b0a090e27016ce0beef380b
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-09-12 22:30:23 +0100

    Adjusting templates structure for better navigation and general look
    - Article page:
      - Adding heading with article title
    - Enabling Edit-article button on each article
    
    - toast - centralizing toast message
    
    - article view - adding login_required decorator

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
@@ -24,6 +25,7 @@ def article(request, article_id):
     return render(request, 'article.html', context)
 
 
+@login_required(redirect_field_name='home')
 def write_article(request):
     """
         Allows content Manager to write his own articles
@@ -43,17 +45,46 @@ def write_article(request):
     return render(request, 'write_article.html', {'form': form})
 
 
-# def edit_article(request, article_id):
-#     return None
+@login_required(redirect_field_name='home')
+def edit_article(request, article_id):
+    """
+        Allows usperuser to edit articles
+        :param request:
+        :param article_id:
+        :return:
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, You are not authorized to do this action.')
+        return redirect(reverse('articles'))
+
+    article = get_object_or_404(Article, pk=article_id)
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST, author_id=request.user.id, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Article changes are saved.')
+            return redirect(reverse('article', args=[article.id]))
+        else:
+            messages.error(request,
+                           'Failed to update Article. Please ensure try again!')
+    else:
+        form = ArticlesForm(instance=article, author_id=article.author.id)
+        messages.info(request, f'Editing {article.title}')
+        return render(request, 'write_article.html', {
+            'form': form,
+            'article': article,
+        })
 
 
+@login_required(redirect_field_name='home')
 def delete_article(request, article_id):
     """ Check if is super user or author before deleting article"""
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, you are not allowed to remove an article')
         return redirect(reverse('home'))
 
-    product = get_object_or_404(Article, pk=article_id)
-    product.delete()
-    messages.success(request, 'Article removed')
+    article = get_object_or_404(Article, pk=article_id)
+    article.delete()
+    messages.success(request, 'Article removed Successfully!')
     return redirect(reverse('articles'))

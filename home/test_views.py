a0e3589822fa11a4a27169a471287351dd3b6abb
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-03 17:04:54 +0100

    Adding About template and view
    - adding some custom css classes
    - Adding new template and view for about page

--- home/test_views.py
+++ home/test_views.py
@@ -7,3 +7,8 @@ class TestHomeViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_get_about_page(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')

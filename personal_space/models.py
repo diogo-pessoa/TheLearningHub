3f12c48e616c9934e0e4dc4dc42023849cc1c761
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-11 09:04:35 +0100

    Creating personal_space Models
    - New model for User Details and Bookmarks

--- personal_space/models.py
+++ personal_space/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articles.models import Article
+
+
+class UserBookmarks(models.Model):
+    content_title = models.CharField(max_length=254)
+    content_path = models.CharField(max_length=256, null=True, blank=True)
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
+
+
+class UserDetails(models.Model):
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
+    bookmarks = models.ForeignKey('UserBookmarks', null=True, blank=True,
+                                  on_delete=models.SET_NULL)
+    bio = models.CharField(max_length=800, null=True)
---
+++ personal_space/test_models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from personal_space.models import UserDetails, UserBookmarks
+from django.test import TestCase
+
+
+class TestUserDetails(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='test_user', is_staff=True)
+
+        self.user_details = UserDetails(user=self.user,
+                                        bio="Testing user Bio")
+        self.user_details.save()
+
+        # Creating Bookmarking
+        self.bookmark = UserBookmarks.objects.create(user=self.user, content_title='content_title',
+                                                     content_path='articles/8/')
+        self.bookmark.save()
+
+    def test_create_user_details(self):
+        user_info = UserDetails.objects.get(user=self.user)
+        self.assertEqual(user_info.user, self.user)
+        self.assertIsNotNone(user_info.bio)
+
+    def test_create_user_bookmarks(self):
+        user_bookmarks = UserBookmarks.objects.all()
+        for bookmark in user_bookmarks:
+            self.assertEqual('content_title', bookmark.content_title)
+            self.assertEqual(self.user, bookmark.user)
+
+

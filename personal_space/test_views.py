d8d990e88148280baf94f865813f126c7b23245c
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-17 17:51:12 +0100

    Including UserNote on Profile page view.
    - testing note is available in context
    - including migrations from UserNote change in models.py

--- personal_space/test_views.py
+++ personal_space/test_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from personal_space.models import UserProfile, UserBookmark
+from personal_space.models import UserProfile, UserBookmark, UserNote
 
 
 class TestPersonalSpaceViews(TestCase):
@@ -58,3 +58,27 @@ class TestPersonalSpaceViews(TestCase):
         self.assertEqual(response.context['user_role_information'].first_name, self.user.first_name)
         self.assertEqual(response.context['user_role_information'].last_name, self.user.last_name)
         self.assertEqual(response.context['user_role_information'].is_staff, self.user.is_staff)
+
+    def test_get_user_notes(self):
+        """ Test a user accessing his notes following from the view perspective, as a site member.
+            - User saves a note
+            - Navigates to profile page
+            - User will see has access to what is in the view context
+            note this is an list as a user can have one or more notes.
+        """
+
+        self.client.login(username='john', password='johndoe123')
+
+        user_note = UserNote.objects.create(user=self.user,
+                                            note_content='content_title',
+                                            content_path='/articles/8/',
+                                            note_title="this is the a note from the course")
+        user_note.save()
+
+        response = self.client.get('/personal_space/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile_index.html')
+        self.assertIsInstance(response.context['user_notes'][0], UserNote)
+        self.assertEqual(response.context['user_notes'][0].note_title, user_note.note_title)
+        self.assertEqual(response.context['user_notes'][0].content_path, user_note.content_path)
+        self.assertEqual(response.context['user_notes'][0].note_title, user_note.note_title)
--- personal_space/views.py
+++ personal_space/views.py
@@ -2,17 +2,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
-from personal_space.models import UserBookmark, UserProfile
+from personal_space.models import UserBookmark, UserProfile, UserNote
 
 
 @login_required
 def profile_index(request):
     user_bookmarks = UserBookmark.objects.filter(user=request.user)
     user_profile_info = UserProfile.objects.filter(user=request.user)
+    user_notes = UserNote.objects.filter(user=request.user)
     user_role_information = request.user
     context = {
         "user_bookmarks": user_bookmarks,
         "user_profile_info": user_profile_info,
-        "user_role_information": user_role_information
+        "user_role_information": user_role_information,
+        "user_notes": user_notes
     }
     return render(request, 'profile_index.html', context)

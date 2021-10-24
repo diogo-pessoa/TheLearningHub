4624124b68f93b186f5afa18fab5c6c2cb872ed9
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-24 18:54:29 +0100

    Adding new views to Video_class
    - Allowing staff to delete content
    - view for video_class page
    - Adding test_views.py
      - checking delete only as staff
      - validating login_required decorator in place

--- video_classes/test_views.py
+++ video_classes/test_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+from video_classes.models import VideoClass
+
 
 class TestVideosClassViews(TestCase):
 
@@ -12,8 +14,43 @@ class TestVideosClassViews(TestCase):
         self.user = User.objects.get(username='john')
         self.visitor = User.objects.get(username='visitor')
 
-    def test_content_manager_creates_video_class(self):
+        self.video_class = VideoClass.objects.create(title='Test_video',
+                                                     author=self.user,
+                                                     video_path='/videos/this_is_fun_media')
+
+    def test_content_manager_can_open_video_class_form_page(self):
         self.client.login(username='john', password='johndoe123')
         response = self.client.get(f'/video_class/create')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('new_video_class.html')
+
+    def test_get_video_class_page(self):
+        self.video_class.save()
+        video_class = VideoClass.objects.all()[0]
+        response = self.client.get(f'/video_class/{video_class.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('video_class.html')
+        self.assertEqual(response.context['video_class'].title, self.video_class.title)
+        self.assertIsInstance(response.context['video_class'], VideoClass)
+
+    def test_fail_delete_video_class_unless_logged_in(self):
+        self.video_class.save()
+        video_class = VideoClass.objects.all()[0]
+        response = self.client.get(f'/video_class/delete/{video_class.id}/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_fail_delete_video_class_if_visitor(self):
+        self.video_class.save()
+        self.client.login(username='visitor', password='visitordoe123')
+        video_class = VideoClass.objects.all()[0]
+        response = self.client.get(f'/video_class/delete/{video_class.id}/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRaisesMessage(response, 'Sorry, you are not allowed to remove content')
+
+    def test_as_staff_delete_video_class(self):
+        self.video_class.save()
+        self.client.login(username='john', password='johndoe123')
+        video_class = VideoClass.objects.all()[0]
+        response = self.client.get(f'/video_class/delete/{video_class.id}/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRaisesMessage(response, 'Video Class Deleted successfully!')
--- video_classes/views.py
+++ video_classes/views.py
@@ -1,23 +1,41 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.urls import reverse
+
+from video_classes.models import VideoClass
+
+
 @login_required(redirect_field_name='home')
 def create_video_class(request):
     return render(request, 'new_video_class.html')
 
 
-def video_class(request):
+def video_class(request, video_class_id):
+    videos_class = get_object_or_404(VideoClass, id=video_class_id)
+    context = {
+        'video_class': videos_class,
+    }
 
-    return render(request, 'video_class.html')
+    return render(request, 'video_class.html', context)
 
 
 @login_required(redirect_field_name='home')
-def delete_video_class(request):
-    return None
+def delete_video_class(request, video_class_id):
+    """ Only allows to delete video if staff """
+    if not request.user.is_staff:
+        messages.error(request, 'Sorry, you are not allowed to remove content')
+        return redirect(reverse('articles'))
+
+    video_class = get_object_or_404(VideoClass, pk=video_class_id)
+    video_class.delete()
+    messages.success(request, 'Video Class Deleted successfully!')
+    return redirect(reverse('articles'))
 
 
 @login_required(redirect_field_name='home')
-def edit_video_class(request):
+def edit_video_class(request, video_class_id):
+    #TODO
     return None

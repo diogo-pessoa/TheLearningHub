d87385f5c97c30dfbfd9cdf114fc9dfde247444a
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-24 17:52:40 +0100

    New VideoClass models.py class
    - registering VideoClass model to admin if in development environment
    -

--- video_classes/admin.py
+++ video_classes/admin.py
@@ -1,3 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 
-# Register your models here.
+from video_classes.models import VideoClass
+
+if settings.DEBUG:
+    admin.site.register(VideoClass)
--- video_classes/models.py
+++ video_classes/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from articles.models import Topic
+
+
+class VideoClass(models.Model):
+    topic = models.ForeignKey(Topic, null=True, blank=True,
+                              on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, null=False)
+    video_path = models.CharField(max_length=10000, null=True)
+    description = models.CharField(max_length=256, null=True)
+    restricted_access = models.BooleanField(default=False, null=False, blank=False)
+    draft = models.BooleanField(default=True, null=False, blank=False)
+    created_at = models.DateTimeField(default=timezone.now, null=False)
+
+    def __str__(self):
+        return self.title

e4cd421beff5a00bdf62674c2685955a50608ac5
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-24 18:01:58 +0100

    Adding test_models.py

---
+++ video_classes/test_models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+from video_classes.models import VideoClass
+
+
+class TestVideoClass(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='test_user', is_staff=True)
+
+    def test_create_user_notes(self):
+        video_class = VideoClass.objects.create(
+            author=self.user,
+            title="test_video",
+            video_path='/thisisATestpath/',
+            description="quick description to show on browse for content card page",
+            restricted_access=False,
+            draft=False,
+            created_at=timezone.now()
+        )
+        video_class.save()
+        video_classes_list = VideoClass.objects.all()
+        for video_class in video_classes_list:
+            self.assertEqual('test_video', video_class.title)
+            self.assertEqual(self.user, video_class.author)
+            self.assertEqual(video_class.video_path, '/thisisATestpath/')
+            self.assertEqual(video_class.description, 'quick description to show on browse for content card page')

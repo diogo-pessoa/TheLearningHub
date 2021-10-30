7e36c9eeef7ac86c02f0fb36c83ec069faf1d3cf
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-10-30 12:32:43 +0100

    removing temporary upload view created for testing.

--- video_classes/urls.py
+++ video_classes/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('create_video_class', views.create_video_class, name="create_video_class"),
     path('<int:video_class_id>/', views.video_class, name="video_class"),
     path('delete/<int:video_class_id>/', views.delete_video_class, name='delete'),
-    path('edit/<int:video_class_id>/', views.edit_video_class, name='edit'),
-    path("upload", views.upload, name="upload")
+    path('edit/<int:video_class_id>/', views.edit_video_class, name='edit')
 ]

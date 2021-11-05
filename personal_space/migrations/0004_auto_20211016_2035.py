ecc861c14e384d4e0ff28bf435945bf9fd8fe6a7
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-11-05 12:02:47 +0000

    fixing Travis test migration error

--- personal_space/migrations/0004_auto_20211016_2035.py
+++ personal_space/migrations/0004_auto_20211016_2035.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
     dependencies = [
         ('personal_space', '0003_auto_20211016_1900'),
     ]

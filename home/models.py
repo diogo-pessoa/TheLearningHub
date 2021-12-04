f881496b28aa6f04d79eb2c334052f7ce0ccda39
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-12-04 09:40:47 +0000

    Adding new Model and migrations to home App

--- home/models.py
+++ home/models.py
@@ -13,3 +13,10 @@ def __str__(self):
     return self.title
 
 
+class LearningFileStorage(models.Model):
+    file = models.FileField(null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    page_source = models.CharField(default=None, null=True, max_length=32)
+
+    def __str__(self):
+        return self.file.name

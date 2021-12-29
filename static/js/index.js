8ec240c9c7fe790fc52f5a2fc3396ba96a715a00
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-12-29 14:57:48 +0000

    small fix on element selector for copy to clipboard feature.
    as we can have one or more files Id shouldn't be used as a selector for the copy to clipboard function.

--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,40 @@
-function readClipBoardText() {
-    // navigator.clipboard.readText().then(text => {
-    //     document.getElementById("file_path").innerHTML = text;
-    // })
+function readClipBoardText(buttonElement) {
     /* Get the text field */
-    var copyText = document.getElementById("file_path");
-
+    let copyText = $(buttonElement).siblings('input');
+    let copyTextContent = copyText.val()
     /* Select the text field */
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
+    navigator.clipboard.writeText(copyTextContent);
 }
 
 $('#collapsable_file_form_info').hide();
-$('#file_upload_description').click(function(e) {
-  e.preventDefault();
-  $('#collapsable_file_form_info').toggle('slow');
+$('#file_upload_description').click(function (e) {
+    e.preventDefault();
+    $('#collapsable_file_form_info').toggle('slow');
 });
 
 
 // Form validation for file size in video_class upload form
-$('#video_class_form').submit(function() {
-  if (window.File && window.FileReader && window.FileList && window.Blob) {
-    let file = $('#id_video_path')[0].files[0];
+$('#video_class_form').submit(function () {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        let file = $('#id_video_path')[0].files[0];
 
-    if (file && file.size > 7 * 1024 * 1024) {
-      alert("File " + file.name + " of type " + file.type + " is too big");
-      return false;
+        if (file && file.size > 7 * 1024 * 1024) {
+            alert("File " + file.name + " of type " + file.type + " is too big");
+            return false;
+        }
     }
-  }
 });
 
 // Form validation for file size in upload file form
-$('#file_upload_form').submit(function() {
-  if (window.File && window.FileReader && window.FileList && window.Blob) {
-    let file = $('#id_file')[0].files[0];
+$('#file_upload_form').submit(function () {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        let file = $('#id_file')[0].files[0];
 
-    if (file && file.size > 2 * 1024 * 1024) {
-      alert("File " + file.name + " of type " + file.type + " is too big");
-      return false;
+        if (file && file.size > 2 * 1024 * 1024) {
+            alert("File " + file.name + " of type " + file.type + " is too big");
+            return false;
+        }
     }
-  }
 });
